9de7098ee49b1617cc2f45e7299902ae082ea495
Isaac Del Castillo <isaacdc03@hotmail.com>
2024-09-28 16:58:53 -0400

    Created doctor dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
-import WebcamCapture from './WebcamCapture';
-import Dashboard from './Dashboard';
+import "./App.css";
+import WebcamCapture from "./WebcamCapture";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import DoctorDashboard from "./DoctorDashboard/doctorDashboard";
 
 function App() {
-    return (
-      <>
-        <Dashboard />
-      </>
-    );
-  }
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/doctorDashboard" element={<DoctorDashboard />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
